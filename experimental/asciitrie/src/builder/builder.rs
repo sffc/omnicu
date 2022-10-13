3590c76e7da94d713bbbe0da1050940f1e59c21d
Shane F. Carr <shane@unicode.org>
2022-10-12 23:40:29 -0700

    In-place building

--- experimental/asciitrie/src/builder/builder.rs
+++ experimental/asciitrie/src/builder/builder.rs
@@ -26,50 +26,38 @@ impl<B: AsciiTrieBuilderStore> AsciiTrieBuilder<B> {
         }
     }
 
-    pub fn byte_len(&self) -> usize {
-        self.data.atbs_len()
+    fn prepend_ascii(&mut self, ascii: AsciiByte) -> usize {
+        self.data.atbs_push_front(ascii.get());
+        1
     }
 
-    fn prepend_ascii(&mut self, ascii: AsciiByte) {
-        self.data.atbs_push_front(ascii.get())
-    }
-
-    fn prepend_value(&mut self, value: usize) {
+    fn prepend_value(&mut self, value: usize) -> usize {
         if value > 0b00011111 {
             todo!()
         }
         self.data.atbs_push_front((value as u8) | 0b10000000);
+        1
     }
 
-    fn make_branch(targets: &[(AsciiByte, Self)]) -> Self {
-        let n = targets.len();
+    fn prepend_branch(&mut self, targets_rev: &[(AsciiByte, usize)]) -> usize {
+        let n = targets_rev.len();
         if n > 0b00011111 {
             todo!()
         }
-        let trie_lengths = targets
-            .iter()
-            .map(|(_, builder)| builder.byte_len())
-            .sum::<usize>();
+        let trie_lengths = targets_rev.iter().map(|(_, size)| size).sum::<usize>();
         if trie_lengths > 256 {
             todo!()
         }
-        // 1 for header, N bytes, N jump indices, and all tries
-        let capacity = 1 + n * 2 + trie_lengths;
-        let mut data = B::atbs_with_capacity(capacity);
-        data.atbs_push_back((n as u8) | 0b11000000);
-        for (ascii, _) in targets.iter() {
-            data.atbs_push_back(ascii.get());
+        let mut index = trie_lengths;
+        for (_, size) in targets_rev.iter() {
+            index -= size;
+            self.data.atbs_push_front(index.try_into().unwrap());
         }
-        let mut index = 0;
-        for (_, trie) in targets.iter() {
-            data.atbs_push_back(index.try_into().unwrap());
-            index += trie.byte_len();
+        for (ascii, _) in targets_rev.iter() {
+            self.data.atbs_push_front(ascii.get());
         }
-        for (_, trie) in targets.iter() {
-            data.atbs_extend(&trie.data);
-        }
-        debug_assert_eq!(capacity, data.atbs_len());
-        Self { data }
+        self.data.atbs_push_front((n as u8) | 0b11000000);
+        1 + n * 2
     }
 
     pub fn from_litemap<'a, S>(items: LiteMap<&'a AsciiStr, usize, S>) -> Self
@@ -77,29 +65,32 @@ impl<B: AsciiTrieBuilderStore> AsciiTrieBuilder<B> {
         S: litemap::store::StoreSlice<&'a AsciiStr, usize>,
         for<'l> &'l S::Slice: litemap::store::StoreSlice<&'a AsciiStr, usize, Slice = S::Slice>,
     {
-        Self::create_recursive(items.as_sliced(), 0)
+        let mut result = Self::new();
+        result.create_recursive(items.as_sliced(), 0);
+        result
     }
 
     fn create_recursive<'a, 'b, S: ?Sized>(
+        &mut self,
         items: LiteMap<&'a AsciiStr, usize, &'b S>,
         prefix_len: usize,
-    ) -> Self
+    ) -> usize
     where
         for<'l> &'l S: litemap::store::StoreSlice<&'a AsciiStr, usize, Slice = S>,
         'a: 'b,
     {
         let first: (&'a AsciiStr, usize) = match items.first() {
             Some((k, v)) => (*k, *v),
-            None => return Self::new(),
+            None => return 0,
         };
         let mut initial_value = None;
+        let mut total_size = 0;
         let items = if first.0.len() == prefix_len {
             initial_value = Some(first.1);
             items.get_indexed_range(1..items.len()).unwrap()
         } else {
             items.as_sliced()
         };
-        let mut result;
         if !items.is_empty() {
             let mut i = items.len() - 1;
             let mut j = items.len();
@@ -113,33 +104,30 @@ impl<B: AsciiTrieBuilderStore> AsciiTrieBuilder<B> {
                     .ascii_at(prefix_len)
                     .unwrap();
                 if c != current_ascii {
-                    let inner = Self::create_recursive(
-                        items.get_indexed_range(i..j).unwrap(),
-                        prefix_len + 1,
-                    );
-                    children.insert(0, (current_ascii, inner));
+                    let inner = self
+                        .create_recursive(items.get_indexed_range(i..j).unwrap(), prefix_len + 1);
+                    total_size += inner;
+                    children.push((current_ascii, inner));
                     current_ascii = c;
                     j = i;
                 }
                 i -= 1;
             }
             let last_child =
-                Self::create_recursive(items.get_indexed_range(i..j).unwrap(), prefix_len + 1);
+                self.create_recursive(items.get_indexed_range(i..j).unwrap(), prefix_len + 1);
+            total_size += last_child;
             if children.is_empty() {
                 // All strings start with same byte
-                result = last_child;
-                result.prepend_ascii(current_ascii);
+                total_size += self.prepend_ascii(current_ascii);
             } else {
                 // Need to make a branch node
-                children.insert(0, (current_ascii, last_child));
-                result = Self::make_branch(&children);
+                children.push((current_ascii, last_child));
+                total_size += self.prepend_branch(&children);
             }
-        } else {
-            result = Self::new();
         }
         if let Some(value) = initial_value {
-            result.prepend_value(value);
+            total_size += self.prepend_value(value);
         }
-        result
+        total_size
     }
 }
